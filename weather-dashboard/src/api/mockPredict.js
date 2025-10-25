93b0649814ae69c8fbeadeb5228f2c16f02e60d8
Randy TAR <randy@tar.com>
2025-10-26 00:19:47 +0100

    Added get_weather_data_by_city and updated a city weather  endpoint

--- weather-dashboard/src/api/mockPredict.js
+++
@@ -1,9 +0,0 @@
-export const fetchPrediction = async (location) => {
-  await new Promise((res) => setTimeout(res, 500));
-  return {
-    Earthquake: ["low", "medium", "high"][Math.floor(Math.random() * 3)],
-    Flood: ["low", "medium", "high"][Math.floor(Math.random() * 3)],
-    Storm: ["low", "medium", "high"][Math.floor(Math.random() * 3)],
-    Wildfire: ["low", "medium", "high"][Math.floor(Math.random() * 3)]
-  };
-};
